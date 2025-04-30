85bc8c50dfb264aaf5435838f0e9677854c396ec
Emmanuel <asamoahe304@gmail.com>
2025-04-30 14:37:06 +0000

    added clsx and tailwind merge utility

--- playground/App.tsx
+++ playground/App.tsx
@@ -6,7 +6,7 @@ export default function App() {
     return (
         <div className="p-6">
             <h1 className="text-xl font-bold mb-4 text-red-400">PrimeReact + Tailwind Test</h1>
-            <Button label="Tailwind Styled" />
+            <Button label="Tailwind Styled" severity='info'/>
         </div>
     )
 }
---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+import { ButtonPT } from "./button/Button"
+
+export const PrimeWind = {
+    button: ButtonPT
+}
