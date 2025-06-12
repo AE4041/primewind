3da5d2ad2b67c13e0bc2cceddc44c24d30941600
Emmanuel <asamoahe304@gmail.com>
2025-06-12 23:07:44 +0000

    added checkbox styles

--- playground/App.tsx
+++ playground/App.tsx
@@ -1,10 +1,15 @@
+import React, { useState } from "react";
+import { Checkbox } from "primereact/checkbox";
 import {Button} from 'primereact/button'
 import "./style.css"
 
 export default function App() {
+    const [checked, setChecked] = useState<boolean>(false);
+
     return (
         <div className="p-6">
             <Button label="Tailwind Styled"  severity="success"  />
+            <Checkbox onChange={e => setChecked(e.checked)} checked={checked}></Checkbox>
         </div>
     )
 }
--- src/components/checkbox/Checkbox.ts
+++ src/components/checkbox/Checkbox.ts
@@ -3,18 +3,17 @@ import { classNames } from "../../util/twMerge"
 
 export const CheckboxPT = {
     root: {
-        className: classNames('shrink-0 relative flex h-5 w-5 items-center justify-center border-gray-300 dark:border-gray-700 overflow-hidden border-2 bg-gray-100 transition-shadow duration-300 dark:bg-gray-800 rounded-md')
+        className: classNames('shrink-0 relative flex h-5 w-5 items-center justify-center border-gray-300 overflow-hidden border-2 bg-gray-100 transition-shadow duration-300  rounded-md')
     },
     input: {
         className: classNames('peer absolute start-0 top-0 z-[3] h-full w-full cursor-pointer appearance-none ')
     },
     box: ({ props, context }: any) => ({
         className: classNames(
-            'flex items-center justify-center',
-            'border-2 w-6 h-6 text-gray-600 rounded-lg transition-colors duration-200',
+            'absolute top-0 start-0 z-[1] block h-full w-full scale-0 transition-transform duration-300 peer-checked:scale-[1.1] bg-muted-300 dark:bg-muted-700 rounded-md',
             {
-                'border-gray-300 bg-white dark:border-blue-900/40 dark:bg-gray-900': !context.checked,
-                'border-blue-500 bg-blue-500 dark:border-blue-400 dark:bg-blue-400': context.checked
+                'border-gray-300 bg-white dark:border-gray-900/40 dark:bg-gray-900': !context.checked,
+                'border-gray-500 bg-gray-300 dark:border-gray-400 dark:bg-[#334155]/50': context.checked
             },
             {
                 'hover:border-blue-500 dark:hover:border-blue-400 focus:outline-none focus:outline-offset-0 focus:shadow-[0_0_0_0.2rem_rgba(191,219,254,1)] dark:focus:shadow-[inset_0_0_0_0.2rem_rgba(147,197,253,0.5)]': !props.disabled,
@@ -22,5 +21,5 @@ export const CheckboxPT = {
             }
         )
     }),
-    icon: 'w-4 h-4 transition-all duration-200 text-white text-base dark:text-gray-900'
+    icon: 'w-4 h-4 relative start-0 z-[2] transition-all duration-200 text-gray-900 text-base dark:text-gray-900'
 }
